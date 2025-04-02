8ef25d285f313f0f20c5b27d3bb722ebaa6aa477
Jon Shui <jonshui@jons-mbp-2.myfiosgateway.com>
2025-04-02 15:29:39 -0400

    format_list

--- StarWarsAPI.py
+++ StarWarsAPI.py
@@ -12,8 +12,8 @@ def retrieve_json(url):
 '''
 This function loops through every character in the json file, 
 checks if species is specified as first item in list struc, else label the character as "Miscellaneous", 
-and adds key value pair of specicies and name to dictionary.
-(default dict is needed so no KeyError is raised when trying to access a key not already in the dict)
+and adds key value pair of specicies and name to dict.
+(default dict is needed as opposed to a standard dict so no KeyError is raised when trying to access a key not already in the dict)
 '''
 def raw_characters():
     characters_url="https://swapi.dev/api/people/"
@@ -24,11 +24,23 @@ def raw_characters():
             break
         for character in data['results']:
             name = character['name']
-            species = character['species'][0] if character['species'] else "Miscellaneous"
+            species = retrieve_json(character['species'][0])['name'] if character['species'] else "Miscellaneous"
             characters_list[species].append(name)
         characters_url = data.get('next') 
     
     return characters_list
 
+'''
+This function loops through the characters_list dict's key value pairs.
+Prints the key (species) followed by a new line of hypens and the values (names) that correspond
+'''
+def display_list(characters_list):
+    for species, names, in characters_list.items():
+        print(f"{species}: ")
+        print("-" * (len(species)))
+        for name in names:
+            print(f"- {name}")
+        print("\n")
+
 if __name__ == "__main__":
-    print(raw_characters())
+    print(display_list(raw_characters()))
