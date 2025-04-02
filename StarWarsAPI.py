01396e42cb32fdc7b2d0f54523959089a88addf9
Jon Shui <jonshui@jons-mbp-2.myfiosgateway.com>
2025-04-02 13:46:18 -0400

    retrieve json functionality from endpoint

--- StarWarsAPI.py
+++ StarWarsAPI.py
@@ -1,1 +1,14 @@
-print("created proj")
+import requests
+
+def retrieve_json(url):
+    response=requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retrieve json from {url}")
+        return None
+
+   
+
+if __name__ == "__main__":
+    retrieve_json("https://swapi.dev/api/planets/1/")
